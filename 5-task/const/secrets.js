a1905c5d62758c126f472ef19d479a54fdef78f1
Vyachaslau Vasilets <vasilechek9786@gmail.com>
2024-09-17 18:13:54 +0300

    Remove secrets.js from tracking and add to .gitignore

--- 5-task/const/secrets.js
+++
@@ -1,4 +0,0 @@
-export const client_id = "8a181728-8577-4fe0-ad9e-697c49d5bc9b"
-export const client_secret = "eaU8Q~vRxkAoHHgcmmKelSbqRhoUTiaAH0Szra5n"
-export const tenant = "qsolutions055.onmicrosoft.com"
-export const scope = "https://orga295fedb.crm11.dynamics.com/user_impersonation"
